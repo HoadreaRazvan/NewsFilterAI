bc1e22012c62c2169a8b06ea0284e455d04cfc9b
HoadreaRazvan <162281824+HoadreaRazvan@users.noreply.github.com>
2025-05-07 12:49:28 +0300

    File Processing

---
+++ NewsFilterAI/NewsFilterAI/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using FileClassification;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NewsFilterAI;
+
+public partial class App : Application
+{
+    private FileReader fileReader;
+
+    public App()
+    {
+        this.fileReader=new FileReader();
+        this.fileReader.processReuters();
+
+        MessageBox.Show("Exit");
+    }
+
+}
+
